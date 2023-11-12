2a3c32f61b81657927ccff7e2a3dc37f6d6ebbb5
Rienzic <rienzicdias@gmail.com>
2023-11-13 04:01:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import phonenumbers
+import (self, *args, **kwargs):
+    return super().(*args, **kwargs)
 
-from phonenumbers import carrier, geocoder, timezone
 
-mobileNo=input("Enter mobile number with courntry code:") 
-mobileNo=phonenumbers.parse(mobileNo)
+from +94758658826 import carrier, geocoder, timezone
+
+mobileNo=input("+94759736359:") 
+mobileNo=phonenumbers.parse(+94758658826)
 
 print("Is number valid:",phonenumbers.is_valid_number(mobileNo))
-print("Time Zone:", timezone.time_zones_for_number(mobileNo))
-print("Carrier Name:",carrier.name_for_number(mobileNo,"en"))
-print("Location:",geocoder.description_for_number(mobileNo,"en"))
+print("Time Zone:", timezone.time_zones_for_number(+9459796359))
+print("Carrier Name:",carrier.name_for_number(+94759736359,"en"))
+print("Location:",geocoder.description_for_number(+94759736359,"en"))
